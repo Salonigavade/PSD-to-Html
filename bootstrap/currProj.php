580053a77788e86e050f04de70d7cd06c2821816
saloni <salonigvd@gmail.com>
2019-12-23 17:58:14 +0530

    changed file html to php

---
+++ bootstrap/currProj.php
@@ -0,0 +1,54 @@
+<?php
+include('header.html');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="lightbox.min.css">
+</head>
+<script type="text/javascript" src="lightbox-plus-jquery.min.js"></script>
+<body style="background-color: #f9f4eb; margin-right: 0px;margin-bottom: 0px;">
+
+<div class="container">
+	<div class="content">
+		<p class="head-title">Current Projects</p><hr>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="mapouter">
+            <div class="gmap_canvas">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d122283.79110568027!2d74.16882740658711!3d16.708456761663925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1000cdec07a29%3A0xece8ea642952e42f!2sKolhapur%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1576839702358!5m2!1sen!2sin" width="100%" height="500" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+              <a href="https://www.embedgooglemap.net/blog/divi-discount-code-elegant-themes-coupon/"></a>
+            </div>
+          </div>
+				</div>
+      </div><br><br>
+
+			<div class="row">
+				<div class="col-md-3">
+					<figure>
+						<a href="images/banner6.jpg" data-lightbox="saloni"><img src="images/om.jpg" alt=""></a>
+            <figcaption style="margin-left: 50px;">
+              <br><br><a href="#" class="fig">OM Nikam Estate</a>
+            </figcaption>
+              <hr>
+					</figure>
+				</div>
+        <div class="col-md-5"></div>
+        <div class="col-md-4"></div>
+			</div>
+			</div>
+	</div>
+</div>
+
+
+</body>
+<?php
+include('footer.html');
+?>
+</html>
