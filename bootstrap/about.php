33beb8ee5ce76ef3ad4d8a2e87574ad31aeab93f
saloni <salonigvd@gmail.com>
2019-12-23 17:54:12 +0530

    completed about.php

---
+++ bootstrap/about.php
@@ -0,0 +1,53 @@
+<?php
+include('header.html');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="lightbox.min.css">
+</head>
+<body>
+<div class="container" style="min-height: 450px;">
+	<div class="row">
+		<div class="col-md-8">
+			<div style="color: #804000; font-size: 20px; font-weight: bold; margin-top: 10px;">Company Overview</div><hr>
+			<p>Incepted in 1993, the M/S. Jaysing G. Hujare(Jadhav) today, is a strong, multifaceted, multi-disciplinary and vibrant organization, committed to delighting our customers through innovation. We strive to achieve the highest degree of technical excellence while maintaining the finest standards of business practices in all our endeavours. It is this trait that has always attracted the most talented people to our company. While we seek to bring in newness and fresh energy into the organization, we’ve never under-valued our old relationships. All the stakeholders who have been with us through our journey and those who keep joining the family are a standing testimony of our beliefs. Such strong teamwork, dedication and sheer hard work have ensured that we have won a major industry award every year since the day we started. For us, this is just the beginning.</p>
+		</div>
+		<div class="col-md-4">
+			<div style="border-left: 1px solid #7f6942;color: #fff; width: 50%;">
+				<div class="abt1" >
+					<div class="abt">
+						<a href="https://www.jaysinghujare.com/company-overview/">Company Overview</a>
+					</div>
+					<div class="abt">
+						<a href="https://www.jaysinghujare.com/company-overview/director-message/" title="Director Message">Director Message</a>
+
+					</div>
+					<div class="abt">
+						<a href="https://www.jaysinghujare.com/company-overview/board/" title="Board">Board</a>
+
+					</div>
+					<div class="abt">
+						<a href="https://www.jaysinghujare.com/company-overview/quality-policy/" title="Quality Policy">Quality Policy</a>
+
+					</div>
+					<div class="abt">
+						<a href="https://www.jaysinghujare.com/company-overview/service-we-provide/" title="Service We Provide">Service We Provide</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+<?php
+include('footer.html');
+?>
+</html>
