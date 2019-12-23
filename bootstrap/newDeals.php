853d58d3c55a07a9242d39117fd3975ba269a1d4
saloni <salonigvd@gmail.com>
2019-12-23 17:58:51 +0530

    changed file html to php

---
+++ bootstrap/newDeals.php
@@ -0,0 +1,78 @@
+<?php
+include('header.html');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body style="background-color: #f9f4eb; margin-right: 0px;margin-bottom: 0px;">
+
+<div class="container">
+	<div class="content">
+		<p class="head-title">New Deals >> OM Nikam Estate</p><hr>
+			<div class="row">
+				<div class="col-md-6">
+					<figure>
+						<a href="#"><img src="images/banner-small.jpg" alt=""></a>
+						<figcaption style="margin-top: 10px; margin-left: 50px;">
+							<a href="https://jaysinghujare.com/current-projects/om-nikam-estate/overview/" > A Project by Unity Promoters and Builders </a>
+						</figcaption>
+					</figure>
+				</div>
+				<div class="col-md-6">
+					<p style="font-size: 12px;">
+						<strong>Company : </strong>
+						<strong>Unity promoters and Builders pvt. ltd.</strong>
+					</p>
+					<p style="font-size: 12px;">
+						<strong>Address : </strong>
+						1360 'B' Ward, Vishnu Plaza, Khari corner,<br>
+						New Mahadwar Road, Kolhapur - 416012
+					</p>
+					<p style="font-size: 12px;">
+						<strong>City : </strong>
+						Kolhapur
+					</p>
+					<p style="font-size: 12px;">
+						<strong>For Booking : </strong>
+						8745269352
+					</p>
+					<p style="font-size: 12px;">
+						<strong>Email : </strong>
+						<a href="#">saloni@gmail.com</a>
+					</p>
+				</div>	
+			</div><br>
+
+				<p class="" style="font-size: 16px"><a style="font-size: 16px" href="#">OM Nikam Estate</a> Is located in the heart of kolhapur city, 1.5 km from Mahalaxmi temple, near about Rankala Talav in Devakar panad.</p><br>
+
+			<div class="row">
+				<div class="col-md-6">
+					<figure>
+						<a href="#"><img src="images/om.jpg" alt=""></a>
+					</figure>
+				</div>
+				<div class="col-md-6"></div>
+						<p style="font-size: 20px;">2BHK Delux Flats</p>
+						<p><strong>Site address :</strong>
+						Om Nikam Esate, 930/3, Devakar Panad, near Nikam Park, Kolhapur.
+					</p>
+
+			</div>
+			</div>
+	</div>
+</div>
+
+
+
+</body>
+<?php
+include('footer.html');
+?>
+</html>
