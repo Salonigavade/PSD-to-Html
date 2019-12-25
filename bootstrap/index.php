8845c739b0f3692ed0612c010de57223e7a38e27
saloni <salonigvd@gmail.com>
2019-12-25 17:55:38 +0530

    font, images and demo html files

--- bootstrap/index.php
+++ bootstrap/index.php
@@ -60,16 +60,17 @@ include('header.php');
                       <h3>Director Message</h3>
                       <hr>
                       <p>It's heartening to see some signal of global economic revival. Though too early to say so, but I am optimistic about the revival of economy earlier than estimated. With our persistent efforts I am happy to see the</p>
+                      <p class="read"><a href="#">read more..</a></p>
                     </div>
                   </div> 
                   <div class="col-md-4 col-sm-12 col-xs-12 l">
                      <h3>Why us..?</h3>
                      <hr>
                      <ul class="link1">
-                       <li><a title="Click here to view new Deals" href="https://jaysinghujare.com/new-deals/">>>New Deal </a></li>
-                       <li><a title="Read what service we provide" href="https://jaysinghujare.com/about-us/service-we-provide/">>>Service </a></li>
-                       <li><a title="Read what quality we serves." href="https://jaysinghujare.com/company-overview/quality-policy/">>>Quality </a></li>
-                       <li><a title="Please don't shy to Contact us" href="https://jaysinghujare.com/contact-us/">>>Want help </a></li>
+                       <li><a title="Click here to view new Deals" href="https://jaysinghujare.com/new-deals/"><img src="images/arrow.gif"> New Deal </a></li>
+                       <li><a title="Read what service we provide" href="https://jaysinghujare.com/about-us/service-we-provide/"><img src="images/arrow.gif"> Service </a></li>
+                       <li><a title="Read what quality we serves." href="https://jaysinghujare.com/company-overview/quality-policy/"><img src="images/arrow.gif"> Quality </a></li>
+                       <li><a title="Please don't shy to Contact us" href="https://jaysinghujare.com/contact-us/"><img src="images/arrow.gif"> Want help </a></li>
                      </ul>
                   </div> 
                   <div class="col-md-4 col-sm-12 col-xs-12">
